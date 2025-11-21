a3e28908742d77b4c9ee89a7b291353e71399e25
Tomasz Beneś <tbenes@anaconda.com>
2025-11-21 14:30:30 +0100

    remove nocona flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,12 @@ cd "${SRC_DIR}"
 # Set PROTOC so prost-build can find protoc
 export PROTOC=$(command -v protoc)
 
+# Remove -march=nocona from CFLAGS/CXXFLAGS to avoid conflict with AVX2 code
+# The build.rs script sets -march=haswell for AVX2 support, but conda-build
+# sets -march=nocona which doesn't support AVX2 instructions
+export CFLAGS=$(echo "$CFLAGS" | sed 's/-march=nocona//g')
+export CXXFLAGS=$(echo "$CXXFLAGS" | sed 's/-march=nocona//g')
+
 cargo build --release --bin qdrant
 
 mkdir -p "${PREFIX}/bin"
